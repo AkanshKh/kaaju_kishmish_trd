5648dad384e1b4b0d25917a8aef3fe4ceaa8daf8
ShreyansGarg <113030755+ShreyansGarg@users.noreply.github.com>
2025-12-20 16:32:41 +0530

    Implement nCr calculation with factorials and inverses

---
+++ src/5.Mathematics/5.nCr_Shrey.cpp
@@ -0,0 +1,42 @@
+const ll MAXN = 1e7;
+const ll MOD = 1e9 + 7;
+
+vector<ll> fac(MAXN + 1);
+vector<ll> inv(MAXN + 1);
+
+/** Computes x^y modulo p in O(log p) time. */
+ll exp(ll x, ll y, ll p)
+{
+    ll res = 1;
+    x %= p;
+    while (y)
+    {
+        if (y & 1)
+        {
+            res *= x;
+            res %= p;
+        }
+        x *= x;
+        x %= p;
+        y >>= 1;
+    }
+    return res;
+}
+
+void factorial() {
+    fac[0] = 1;
+    for (ll i = 1; i <= MAXN; i++) {
+        fac[i] = ( fac[i - 1] * i ) % MOD;
+    }
+}
+
+void inverses() {
+    inv[MAXN] = exp(fac[MAXN], MOD - 2, MOD);
+    for (ll i = MAXN; i >= 1; i--) {
+        inv[i - 1] = (inv[i] * i) % MOD;
+    }
+}
+
+ll choose(int n, int r) { 
+    return fac[n] * inv[r] % MOD * inv[n - r] % MOD;
+}
